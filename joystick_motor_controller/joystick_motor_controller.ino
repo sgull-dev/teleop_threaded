64b0a0de6b0831abfadc4a996cc834a781e5ec9f
Lauri <nurmenlauri1@gmail.com>
2024-10-01 17:21:10 +0300

    Code Joystick Usage+Motor Controls based on joystick x/y.

--- joystick_motor_controller/joystick_motor_controller.ino
+++ joystick_motor_controller/joystick_motor_controller.ino
@@ -1,67 +1,117 @@
-#include <Arduino.h>
+#include "Arduino.h"
+#define DEAD_ZONE 15
 
-const int trigPin = 22;
-const int echoPin = 23;
+// Joystick Pins
+// TODO: Remove and replace by Radio/BT Input signal
+const int joystickX = A4;
+const int joystickY = A5;
 
-unsigned long duration;
-float distance;
+// L298N Motor Controller Pins
+const int left1 = 2;
+const int left2 = 4;
+const int leftPWM = 3;
 
-const int left1 = 26;
-const int left2 = 25;
-const int right1 = 33;
-const int right2 = 32; 
+const int right1 = 5;
+const int right2 = 7;
+const int rightPWM = 6;
+
+// Data
+int x = 0;
+int y = 0;
 
 void setup() {
-  // Setup Distance Sensor
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  Serial.begin(9600);
-  //Setup motor control pins
-  pinMode(left1, OUTPUT);
-  pinMode(left2, OUTPUT);
-  pinMode(right1, OUTPUT);
-  pinMode(right2, OUTPUT);
+    // Set all the motor control pins to outputs
+    pinMode(leftPWM, OUTPUT);
+    pinMode(rightPWM, OUTPUT);
+    pinMode(left1, OUTPUT);
+    pinMode(left2, OUTPUT);
+    pinMode(right1, OUTPUT);
+    pinMode(right2, OUTPUT);
+    
+    // Turn off motors - Initial state
+    digitalWrite(left1, LOW);
+    digitalWrite(left2, LOW);
+    digitalWrite(right1, LOW);
+    digitalWrite(right2, LOW);
+
+    //Debugging
+    //Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(trigPin, LOW);
+void loop () {
+    // Joystick value in int [0, 1023]
+    x = analogRead(joystickX);
+    y = analogRead(joystickY);
 
-  delayMicroseconds(2);
+    // If Input is less than 511-DEAD_ZONE, it should move back
+    // Else if Input is more than 511 + DEAD_ZONE, it should move forward
+    // Else stationary
+    // If moving, convert the 512-range value into PWM signal for motor speed control
 
-  digitalWrite(trigPin, HIGH);
+    // Left Motor Control
+    if (x < 511 - DEAD_ZONE) {
+	left_back();
+	int speed = (511-x)/2;
+	analogWrite(leftPWM, speed);
+    } else if (x > 511 + DEAD_ZONE) {
+	left_forward();
+	int speed = (x-512)/2;
+	analogWrite(leftPWM, speed);
+    } else {
+	left_stop();
+    }
 
-  delayMicroseconds(10);
+    // Right motor control 
+    if (y < 511 - DEAD_ZONE) {
+        right_back();
+        int speed = (511 - y) / 2;
+        analogWrite(rightPWM, speed);
+    } else if (y > 511 + DEAD_ZONE) {
+        right_forward();
+        int speed = (y - 512) / 2;
+        analogWrite(rightPWM, speed);
+    } else {
+        right_stop();
+    }
 
-  digitalWrite(trigPin, LOW);
-
-  duration = pulseIn(echoPin, HIGH);
-
-  distance = (duration*0.0343)/2;
-
-  Serial.print("Distance: ");
-  Serial.println(distance);
-  
-  if (distance > 50) {
-    go_forward();
-  } else {
-    turn_right();
-  }
-  delay(33);
+    /*
+    Serial.print("Joystick X: ");
+    Serial.println(x);
+    Serial.print("Joystick Y: ");
+    Serial.println(y);
+    */
+    
+    delay(100);  
 }
 
-void go_forward() {
-  digitalWrite(left1, HIGH);
-  digitalWrite(left2, LOW);
-
-  digitalWrite(right1, HIGH);
-  digitalWrite(right2, LOW);
+// Functions for controlling motor directions
+void left_forward() {
+    digitalWrite(left1, HIGH);
+    digitalWrite(left2, LOW);
 }
 
-void turn_right() {
-  digitalWrite(left1, LOW);
-  digitalWrite(left2, HIGH);
+void left_back() {
+    digitalWrite(left1, LOW);
+    digitalWrite(left2, HIGH);
 
-  digitalWrite(right1, HIGH);
-  digitalWrite(right2, LOW);
+}
+
+void left_stop() {
+    digitalWrite(left1, LOW);
+    digitalWrite(left2, LOW);
+}
+
+void right_forward() {
+    digitalWrite(right1, HIGH);
+    digitalWrite(right2, LOW);
+}
+
+void right_back() {
+    digitalWrite(right1, LOW);
+    digitalWrite(right2, HIGH);
+}
+
+void right_stop() {
+    digitalWrite(right1, LOW);
+    digitalWrite(right2, LOW);
 }
